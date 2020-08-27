b0344490fa44e13294356caf511906d219733ad2
Hachem Said <labsar.ahmed@outlook.fr>
2020-08-27 01:54:17 -0400

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="row">
+      <div className="col-12">
+        <div className="jumbotron jumbotron-fluid text-center">
+          <div className="container">
+            <h1 className="display-4">Employee Directory</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Header;
